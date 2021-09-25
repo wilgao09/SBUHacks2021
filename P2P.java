a47e3cc955f4b7eb72e56c1acef94f03a55d4868
wilgao09 <william.gao09@gmail.com>
2021-09-25 12:47:30 -0400

    gd

--- P2P.java
+++ P2P.java
@@ -1,7 +1,9 @@
 
 
 import java.net.Inet4Address;
+import java.nio.file.Files;
 import java.io.File;
+import java.io.IOException;
 
 
 public class P2P {
@@ -21,7 +23,7 @@ public class P2P {
         GET_IP
     }
 
-    private static Body b = new Body(8080, false);
+    private static Body b;
 
     public static boolean isValidIP(String s) {
         try {
@@ -36,12 +38,47 @@ public class P2P {
         File f = new File(s);
         return f.exists();
     }
-    public static boolean isValidFolderOut(String s) {
+    public static boolean isValidFolderIn(String s) {
         File f = new File(s);
         return f.canWrite();
     }
 
+    //assume b works
     public static void sendFile(UIState s ) {
+        boolean vIP = isValidIP(s.ip);
+        boolean vPort = ( Integer.parseInt(s.port) < 20000);
+        boolean vFOUT = isValidFileOut(s.fOut);
+        boolean vFIN = isValidFolderIn(s.fIn);
+
+        if (vIP && vPort && vFOUT && vFIN) {
+            if (b.isListening()) {
+                b.breakListen();
+            }
+            b.setDestination(s.ip);
+            File f = new File(s.fIn);
+            try {
+                byte[] data = Files.readAllBytes(f);
+            } catch (IOException e) {
+                System.out.println("FAILED FOR UNKNOWN REASON");
+                System.exit(1);
+            } catch (OutOfMemoryError e) {
+                System.out.println("NOT ENOUGH MEMORY");
+                System.exit(1);
+            } catch (Exception e) {
+                System.out.println("Unhandled");
+                System.exit(1);
+            }
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        //sender
+        try {
+            b = new Body(8080, false);
+        } catch (Exception e) {}
+
+        
 
     }
 }
